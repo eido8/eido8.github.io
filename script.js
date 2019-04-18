2006ff6e9864e2dbf68d5055ea1861a6575603fe
eidoade <48271387+eido8@users.noreply.github.com>
2019-04-18 01:40:13 -0500

    navbar design 4/18
    Added navbar animations
    Changed sample album art designs
    Changed watch div design

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+//
+// window.onload = () => {
+//   let topNavElems = document.querySelectorAll('#myTopnav > a');
+//   for (let elem of topNavElems) {
+//     let navLine = document.querySelector(".navLine");
+//     elem.addEventListener('mouseenter', () => {
+//       console.log("asdf");
+//       let rect = elem.getBoundingClientRect();
+//       navLine.style.width = (rect.left + rect.right)/2 + "px";
+//     });
+//     elem.addEventListener('mouseleave', () => {
+//       navLine.style.width = "0px";
+//     });
+//   }
+// }

0a16d61035c02319357a07a0d29bbabc6a7d5b6b
eido <48271387+eido8@users.noreply.github.com>
2019-10-01 02:16:25 +0900

    Changed overall design

--- script.js
+++ script.js
@@ -6,24 +6,4 @@ function myFunction() {
     x.className = "topnav";
   }
 }
-//
-// window.onload = () => {
-//   let topNavElems = document.querySelectorAll('#myTopnav > a');
-//   for (let elem of topNavElems) {
-//     let navLine = document.querySelector(".navLine");
-//     elem.addEventListener('mouseenter', () => {
-//       console.log("asdf");
-//       let rect = elem.getBoundingClientRect();
-//       navLine.style.width = (rect.left + rect.right)/2 + "px";
-//     });
-//     elem.addEventListener('mouseleave', () => {
-//       navLine.style.width = "0px";
-//     });
-//   }
-// }
 
-$("#listenNav").click(function() {
-    $('html, body').animate({
-        scrollTop: $(".bgimg-1").offset().top
-    }, 2000);
-});
